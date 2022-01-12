af72e81076c93a35bd2c315c8db770f27c9e8dc0
Patricio Carames <patriciocarames@gmail.com>
2022-01-12 12:22:07 -0300

    mi primer commit

---
+++ mi-app/src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import "./navbar.css";
+
+function Navbar(){
+
+const estiloDelNavbar = {}
+
+    return(
+        <div className="navbar">
+            <nav style={estiloDelNavbar}>
+                <ul>
+                    <li>lorem</li>
+                    <li>ipsum</li>
+                    <li>dolor</li>                
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar; 
